fdb891663eff6f0f2c727b2151dfd07a2adb97f3
unknown <shanu1998end@gmail.com>
2026-02-28 05:45:38 +0500

    Add background image to all Retrofit Calculator pages

--- Retrofit Calculator/src/app/components/Layout.tsx
+++ Retrofit Calculator/src/app/components/Layout.tsx
@@ -3,7 +3,13 @@ import { Sidebar } from "./Sidebar";
 
 export function Layout() {
   return (
-    <div className="flex min-h-screen bg-[#F8FAFC]">
+    <div 
+      className="flex min-h-screen bg-cover bg-center bg-no-repeat bg-fixed"
+      style={{
+        backgroundImage: 'url(/background.png)',
+        backgroundColor: '#F8FAFC'
+      }}
+    >
       <Sidebar />
       <div className="flex-1">
         <Outlet />
